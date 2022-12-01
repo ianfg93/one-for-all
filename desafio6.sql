11feb812c45e71898b1ecd46a950fafa74ab0fde
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-12-01 17:20:44 -0300

    correcao do 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT 
-    ROUND(MIN(valor_plano), 2) AS faturamento_minimo,
-    ROUND(MAX(valor_plano), 2) AS faturamento_maximo,
-    ROUND(AVG(valor_plano), 2) AS faturamento_medio,
-    ROUND(SUM(valor_plano), 2) AS faturamento_total
+    FORMAT(MIN(valor_plano), 2) AS faturamento_minimo,
+    FORMAT(MAX(valor_plano), 2) AS faturamento_maximo,
+    FORMAT(AVG(valor_plano), 2) AS faturamento_medio,
+    FORMAT(SUM(valor_plano), 2) AS faturamento_total
 FROM
     SpotifyClone.plano AS a
         INNER JOIN
